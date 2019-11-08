fd480c23264471389c3376d4ca94f07289699afe
sanderdebr <sander606@gmail.com>
2019-11-08 07:01:19 +0100

    'commit'

---
+++ smartbrain/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      hoi
+      <Nav />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ smartbrain/src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.scss';
+
+const Nav = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p>tekst</p>
+        </nav>
+    );
+}
+
+export default Nav;
