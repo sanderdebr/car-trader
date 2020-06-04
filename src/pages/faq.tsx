f55490b2971b9692975c889ce204b72f9e4e94d6
sanderdebr <sander606@gmail.com>
2020-06-04 12:44:12 +0200

    sqlite node downgrade

---
+++ src/pages/faq.tsx
@@ -0,0 +1,41 @@
+import { GetStaticProps } from "next";
+import { FaqModel } from "../../api/Faq";
+import { openDB } from "../openDB";
+import {
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  Typography,
+  ExpansionPanelDetails,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+interface FaqProps {
+  faq: FaqModel[];
+}
+
+export default function Faq({ faq }: FaqProps) {
+  return (
+    <div>
+      {faq.map((f) => (
+        <ExpansionPanel key={f.id}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>{f.question}</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>{f.answer}</Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      ))}
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const db = await openDB();
+  const faq = await db.all("SELECT * FROM FAQ ORDER BY createDate DESC");
+  return { props: { faq } };
+};
