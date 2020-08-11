5e1e205f114d26ccf86e4a10adcb046dd4692f58
sanderdebr <sander606@gmail.com>
2020-08-11 10:48:16 +0200

    formik form

--- database-test.js
+++ database-test.js
@@ -1,19 +1,19 @@
-const sqlite = require('sqlite');
-const sqlite3 = require('sqlite3');
+const sqlite = require("sqlite");
+const sqlite3 = require("sqlite3");
 
 async function setup() {
   const db = await sqlite.open({
-    filename: 'cars.sqlite',
+    filename: "cars.sqlite",
     driver: sqlite3.Database,
   });
 
-  await db.migrate({ force: 'last' });
+  await db.migrate({ force: "last" });
 
-  const faq = await db.all('SELECT * FROM FAQ ORDER BY createDate DESC');
-  console.log('ALL faq', JSON.stringify(faq, null, 2));
+  const faq = await db.all("SELECT * FROM FAQ ORDER BY createDate DESC");
+  // console.log('ALL faq', JSON.stringify(faq, null, 2));
 
-  const cars = await db.all('SELECT * FROM Car');
-  console.log('ALL CARS', JSON.stringify(cars, null, 2));
+  const cars = await db.all("SELECT * FROM Car");
+  // console.log('ALL CARS', JSON.stringify(cars, null, 2));
 }
 
 setup();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,112 @@
-export default function Home() {
-  return <h1>Hello Berend</h1>;
+import { getMakes, Make } from "../database/getMakes";
+import { GetServerSideProps } from "next";
+import { Formik, Form, Field } from "formik";
+import { Paper, Grid, makeStyles } from "@material-ui/core";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { useRouter } from "next/router";
+
+export interface HomeProps {
+  makes: Make[];
 }
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: "auto",
+    maxWidth: 500,
+    padding: theme.spacing(3),
+  },
+}));
+
+const prices = [500, 1000, 5000, 15000, 25000, 50000, 250000];
+
+export default function Home({ makes }: HomeProps) {
+  const classes = useStyles();
+  const { query } = useRouter();
+
+  const initialValues = {
+    make: query.make || "all",
+    model: query.model || "all",
+    minPrice: query.minPrice || "all",
+    maxPrice: query.maxPrice || "all",
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      {({ values }) => (
+        <Form>
+          <Paper elevation={5} className={classes.paper}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel id="search-make">Make</InputLabel>
+                  <Field
+                    name="make"
+                    as={Select}
+                    labelId="search-make"
+                    label="Make"
+                  >
+                    <MenuItem value="all">
+                      <em>All Makes</em>
+                    </MenuItem>
+                    {makes.map((make) => (
+                      <MenuItem value={make.make}>
+                        {`${make.make} (${make.count})`}
+                      </MenuItem>
+                    ))}
+                  </Field>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                MODEL
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel id="search-min-price">Min Price</InputLabel>
+                  <Field
+                    name="minPrice"
+                    as={Select}
+                    labelId="search-min-price"
+                    label="Min Price"
+                  >
+                    <MenuItem value="all">
+                      <em>No min</em>
+                    </MenuItem>
+                    {prices.map((price) => (
+                      <MenuItem value={price}>{price}</MenuItem>
+                    ))}
+                  </Field>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel id="search-max-price">Max Price</InputLabel>
+                  <Field
+                    name="maxPrice"
+                    as={Select}
+                    labelId="search-max-price"
+                    label="Max Price"
+                  >
+                    <MenuItem value="all">
+                      <em>No max</em>
+                    </MenuItem>
+                    {prices.map((price) => (
+                      <MenuItem value={price}>{price}</MenuItem>
+                    ))}
+                  </Field>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const makes = await getMakes();
+  return { props: { makes } };
+};
